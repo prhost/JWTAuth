d6720ce3af1d1843218ca4a0a470f2fa50fc996e
Kallef Alexandre <kallef.alexandre@orbital.codes>
2024-07-14 22:04:28 -0300

    fix jwt plugin register plugin user

--- config/plugins.php
+++ config/plugins.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-//    [
-//        'name' => 'Rainlab.User',
-//        'model' => 'RainLab\User\Models\User',
-//        'resolver' => \Prhost\JWTAuth\Classes\Resolvers\RainlabPlugin::class,
-//        'provider' => 'user.auth',
-//    ],
     [
-        'name' => 'Lovata.Buddies',
-        'model' => 'Lovata\Buddies\Models\User',
-        'resolver' => \Prhost\JWTAuth\Classes\Resolvers\BuddiesPlugin::class,
-        'provider' => 'auth.helper',
-    ]
+        'name'     => 'Rainlab.User',
+        'model'    => 'RainLab\User\Models\User',
+        'resolver' => \Prhost\JWTAuth\Classes\Resolvers\RainlabPlugin::class,
+        'provider' => 'user.auth',
+    ],
+//    [
+//        'name' => 'Lovata.Buddies',
+//        'model' => 'Lovata\Buddies\Models\User',
+//        'resolver' => \Prhost\JWTAuth\Classes\Resolvers\BuddiesPlugin::class,
+//        'provider' => 'auth.helper',
+//    ]
 ];
